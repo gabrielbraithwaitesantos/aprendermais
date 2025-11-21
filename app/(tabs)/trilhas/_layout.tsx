1213ba365c4476e4838f1a5b1d71eea9d954dc2d
Gabriel <felipedossantosbgabrielll@gmail.com>
2025-11-21 01:46:36 -0300

    Rename recurso header to Provas e Gabaritos

--- app/(tabs)/trilhas/_layout.tsx
+++ app/(tabs)/trilhas/_layout.tsx
@@ -6,8 +6,13 @@ export default function TrilhasLayout() {
     <Stack>
       <Stack.Screen name="index" options={{ title: 'Trilhas' }} />
       <Stack.Screen name="[id]" options={{ title: 'Trilha' }} />
-      <Stack.Screen name="recurso/[id]" options={{ title: 'Recurso' }} />
+      <Stack.Screen
+        name="recurso/[id]"
+        options={{
+          title: 'Provas e Gabaritos',
+          headerBackTitleVisible: false,
+        }}
+      />
     </Stack>
   );
 }
-
